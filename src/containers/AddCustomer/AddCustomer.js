a7fd14075b5c1e75ba3e9d1b0b5445491b7a062f
thridev.dabburi@thomsonreuters.com <thridev.dabburi@refinitiv.com>
2021-04-07 20:54:37 +0530

    Commiting changes for ejecting

---
+++ src/containers/AddCustomer/AddCustomer.js
@@ -0,0 +1,15 @@
+import { React, Component } from "react";
+import Aux from "../../hoc/Aux";
+
+class AddCustomer extends Component {
+    render() {
+        return(
+            <Aux>
+                <div>Customer</div>
+                <div>CustomerDetailsInput</div>
+            </Aux>
+        );
+    }
+}
+
+export default AddCustomer;
